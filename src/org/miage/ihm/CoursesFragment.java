43b37ad82f2984b281338935d4a24511f221f1c1
MohamedMB <mohamed.maktouf@gmail.com>
2014-02-03 13:12:38 +0100

    Ajout du menu planning et de la liste des courses.

---
+++ src/org/miage/ihm/CoursesFragment.java
@@ -0,0 +1,17 @@
+package org.miage.ihm;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CoursesFragment extends Fragment {
+    public CoursesFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_courses, container, false);
+        return rootView;
+    }
+}
---
+++ src/org/miage/ihm/PlanningFragment.java
@@ -0,0 +1,17 @@
+package org.miage.ihm;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PlanningFragment extends Fragment {
+    public PlanningFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_planning, container, false);
+        return rootView;
+    }
+}
